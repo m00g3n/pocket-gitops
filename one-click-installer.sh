080c01538bc4b2d529bfa66b360cf5dc47bee031
Marcin Witalis <marcin.witalis@sap.com>
2020-11-24 23:50:28 +0100

    update project structurre; refactor

--- one-click-installer.sh
+++ one-click-installer.sh
@@ -1,43 +1,47 @@
 #!/bin/sh
 
-# this format will return only the ip address of the container
+############################ COREDNS PATCH ################################################################
+
 readonly DOCKER_IP_ADDR_TPL='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'
-
 readonly GITEA_IP_ADDRESS=$(docker inspect -f "$DOCKER_IP_ADDR_TPL" /gitea)
-
 readonly REGISTRY_IP_ADDRESS=$(docker inspect -f "$DOCKER_IP_ADDR_TPL" /registry)
 
 sed "s/__GITEA_IP_ADDRESS__/$GITEA_IP_ADDRESS/; s/__REGISTRY_IP_ADDRESS__/$REGISTRY_IP_ADDRESS/" \
-./coredns.template.patch.yaml \
-| tee coredns.patch.yaml
+./coredns.template.patch.yaml > ./kustomize/base/coredns.patch.yaml
 
-kubectl -n kube-system patch cm coredns --patch "$(cat coredns.patch.yaml)"
+############################ CREATE FLUX KNOWN HOSTS CONFIGMAP ############################################
 
-NAMESPACE=${1:-flux}
-HOST=${2:-gitea}
+readonly NAMESPACE=${1:-flux}
+readonly HOST=${2:-gitea}
 
-docker run -i -t --rm --network=pocker-gitops_hyc-demo kroniak/ssh-client ssh-keyscan $HOST \
-| tee ./known_hosts
+# create temp file that will contain known hosts
+readonly tmpfile=$(mktemp /tmp/known_hosts.XXXXXX)
+exec 3>"$tmpfile"
 
-# create namespace if not exists
-kubectl create ns $NAMESPACE \
-    -oyaml \
-    --save-config \
-    --dry-run \
-| kubectl apply -f -
+docker run -i -t --rm \
+--network=hyc-demo \
+kroniak/ssh-client \
+ssh-keyscan $HOST \
+> $tmpfile
 
-# create or replace configmap with known_hosts
 kubectl create configmap flux-ssh-config \
-    -n $NAMESPACE \
-    --from-file=known_hosts \
-    -oyaml \
-    --dry-run \
-| kubectl apply -f -
+-n $NAMESPACE \
+--from-file=known_hosts=$tmpfile \
+-oyaml \
+--dry-run \
+--save-config \
+> ./kustomize/base/flux-ssh-config.yaml
+
+rm "$tmpfile"
+
+############################ ###############################################################################
 
 fluxctl install \
-    --git-user=admin123 \
-    --git-email=me@test.you \
-    --git-url=git@gitea:hyc-gitops/dywan.git \
-    --namespace=flux > ./kustomize/base/flux.yaml
+--git-user=admin123 \
+--git-email=me@test.you \
+--git-url=git@gitea:hyc-gitops/dywan.git \
+--namespace=flux > ./kustomize/base/flux.yaml
+
+############################ ###############################################################################
 
 kubectl apply -k ./kustomize/known_hosts
